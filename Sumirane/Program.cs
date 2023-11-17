7d635bbdab04a895171830fba222f8914989c039
Admin <Admin@28122017KP1>
2023-11-17 23:00:01 +0200

    Add project files.

---
+++ Sumirane/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sumirane
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            for (int i = 0; i <= 100; i++)
+            {
+                sum = sum + i;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
